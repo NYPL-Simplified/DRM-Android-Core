8253e980b010df41f70e505de6ca2cf72f24f154
Mark Raynsford <code@io7m.com>
2021-03-04 11:13:17 +0000

    Add comments

--- org.librarysimplified.drm.core/src/main/java/org/nypl/drm/core/DRMProtectedFile.kt
+++ org.librarysimplified.drm.core/src/main/java/org/nypl/drm/core/DRMProtectedFile.kt
@@ -4,7 +4,21 @@ import org.readium.r2.shared.publication.asset.FileAsset
 import org.readium.r2.shared.publication.asset.PublicationAsset
 import java.io.File
 
+/**
+ * A Readium2 DRM-protected asset.
+ */
+
 class DRMProtectedFile(
+
+  /**
+   * The actual EPUB file.
+   */
+
   val fileAsset: FileAsset,
+
+  /**
+   * The Adobe rights file, assuming there is one.
+   */
+
   val adobeRightsFile: File?
 ) : PublicationAsset by fileAsset
